c51d73657417e8d0621fe694c658c01ed6de74e3
Edgar <epgarzonr@uce.edu.ec>
2025-02-05 22:19:11 -0500

    docs: add Swagger documentation for creating a new order

--- src/main/java/com/mipagina/order_service/controller/OrderController.java
+++ src/main/java/com/mipagina/order_service/controller/OrderController.java
@@ -2,6 +2,10 @@ package com.mipagina.order_service.controller;
 
 import com.mipagina.order_service.model.Order;
 import com.mipagina.order_service.service.IOrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,9 +21,15 @@ public class OrderController {
     @Autowired
     private IOrderService orderService;
 
-
+    @Operation(summary = "Create a new order", description = "Creates a new order in the system based on the provided order details.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Order created successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input provided")
+    })
     @PostMapping
-    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+    public ResponseEntity<Order> createOrder(
+            @Parameter(description = "Order object that contains the details of the new order", required = true)
+            @RequestBody Order order) {
         return new ResponseEntity<>(orderService.createOrder(order), HttpStatus.CREATED);
     }
 }
--- src/main/java/com/mipagina/order_service/model/OrderItem.java
+++ src/main/java/com/mipagina/order_service/model/OrderItem.java
@@ -1,6 +1,7 @@
 package com.mipagina.order_service.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 
 @Entity
@@ -8,13 +9,23 @@ import jakarta.persistence.*;
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unique identifier of the order item", example = "1")
     private Long id;
+
+    @Schema(description = "Unique identifier for the product in the order", example = "2001", required = true)
     private Long productId;
+
+    @Schema(description = "Quantity of the product ordered", example = "2", required = true)
     private Integer quantity;
+
+    @Schema(description = "Unit price of the product", example = "100.25", required = true)
     private Double unitPrice;
+
+    @Schema(description = "Subtotal for the order item (quantity * unit price)", example = "200.50", required = true)
     private Double subtotal;
 
-    @ManyToOne
+
+@ManyToOne
     @JoinColumn(name = "order_id")
     @JsonBackReference
     private Order order;
